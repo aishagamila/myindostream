145fb905e12e525263e128f8c3a34d00ecc5af88
Autda Ploes <aishagamilamusliansyah@gmail.com>
2025-07-23 15:18:33 +0700

    Update Cgvindo.kt

--- Rebahin/src/main/kotlin/com/rebahin/Cgvindo.kt
+++ Rebahin/src/main/kotlin/com/rebahin/Cgvindo.kt
@@ -3,6 +3,6 @@ package com.rebahin
 class Cgvindo : Rebahin() {
 
     // override var mainUrl = "http://62.171.170.1/"
-    override var mainUrl = "https://cgvindo.christmas/"
+    override var mainUrl = "https://cgvindo2.icu"
     override var name = "Cgvindo"
 }
